406f81f8f889627f022dab8b46fbf61b8ec718c2
josepaulo <josepaulo@ceuma.br>
2022-01-29 21:19:59 -0300

    [wip] exibindo os rostos

--- js/main.js
+++ js/main.js
@@ -1,7 +1,44 @@
+const buildPNGGraphGrafic = () => {
+  let graphics = Viva.Graph.View.svgGraphics();
+
+  // This function let us override default node appearance and create
+  // something better than blue dots:
+  graphics.node(function(node) {
+      // node.data holds custom object passed to graph.addNode():
+      var url = '../img/tile001.png';
+
+      return Viva.Graph.svg('image')
+            .attr('width', 24)
+            .attr('height', 24)
+            .link(url);
+  });
+
+  // Usually when you have custom look for nodes, you might want to
+  // set their position in a new way too. placeNode() method serves
+  // this goal:
+  graphics.placeNode(function(nodeUI, pos) {
+      // nodeUI - is exactly the same object that we returned from
+      //   node() callback above.
+      // pos - is calculated position for this node.
+      nodeUI.attr('x', pos.x - 12).attr('y', pos.y - 12);
+  });
+
+  return graphics
+}
+
 function onLoad() {
+  var graph = Viva.Graph.graph();
+  graph.addNode(0, 'eliezer');
+  graph.addNode(1, 'naiara');
+  graph.addNode(2, 'naiara');
+  graph.addNode(3, 'naiara');
+  // graph.addLink(0, 1);
+
+  graphics = buildPNGGraphGrafic()
+
   // Let's construct simple graph:
   // 0 -> 1 -> 2 -> 3 -> 4 -> 5
-  var graph = Viva.Graph.graph();
+
   graph.addLink(0, 1, { connectionStrength: 1 });
   graph.addLink(1, 2, { connectionStrength: 0.8 });
   graph.addLink(2, 3, { connectionStrength: 0.6 });
@@ -11,7 +48,7 @@ function onLoad() {
 //   graph.addLink(5, 6, { connectionStrength: 0.1 });
 
   // let's pin middle node:
-  var middle = graph.getNode(3);
+  var middle = graph.getNode(1);
   middle.isPinned = true;
 
   var idealLength = 90;
@@ -28,11 +65,12 @@ function onLoad() {
   });
 
   // let's pin middle node:
-  var middle = graph.getNode(3);
+  var middle = graph.getNode(1);
   layout.pinNode(middle, true);
 
   var renderer = Viva.Graph.View.renderer(graph, {
-      layout : layout
+      layout : layout,
+      graphics: graphics
   });
 
   renderer.run();
